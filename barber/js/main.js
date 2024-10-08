c90627722802c322e5abf872c46360a76ebf85e0
Abrambarrientos <abrahambarrientos846@gmail.com>
2024-10-07 20:35:38 -0600

    version 1.6

--- barber/js/main.js
+++ barber/js/main.js
@@ -12,3 +12,27 @@ const showMenu = (toggleId, navId) =>{ // Declaramos una constante y le mandamos
 }
 
     showMenu('nav-toggle','nav-menu')
+
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-envuelto .lista-imagenes");
+    const slideButtons = document.querySelectorAll(".slider-envuelto .slide-button");
+    const sliderScrollbar = document.querySelector(".contenedor-slider .slider-scrollbar");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth *direction;
+            imageList.scrollBy({left: scrollAmount, behavior: "smooth"})
+        });
+    });
+
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+    }
+
+
+}
+
+window.addEventListener("load",initSlider);
